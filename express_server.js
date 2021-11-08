ac5249ae9a55cd2c74360484dc509968a3331a40
Zack <zhanbin3155@gmail.com>
2021-11-08 17:55:54 -0500

    Updated server with POST

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Use EJS as its templating engine
 app.set("view engine", "ejs");
+// Make POST data readable
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +31,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// needs to be defined before the GET /urls/:id route
+// as Express will think that new is a route parameter
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', function (req, res) {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -35,6 +44,15 @@ app.get('/urls/:shortURL', function (req, res) {
   res.render("urls_show", templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 8);
+}
